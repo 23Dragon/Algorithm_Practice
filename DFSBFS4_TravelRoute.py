f1227005f28c037ad5fc63a12d159276b5b76f8a
23Dragon <lsy1819@gamil.com>
2021-03-10 15:03:52 +0900

    0310

--- DFSBFS4_TravelRoute.py
+++ DFSBFS4_TravelRoute.py
@@ -1,45 +1,43 @@
 # coding=UTF-8
 from collections import defaultdict
+
 def solution(tickets):
-    def init_path():
+    def init_gragh():
         rt = defaultdict(list)
         for key, value in tickets:
             rt[key].append(value)
         return rt
-
+    
     def dfs(key, path):
-        if N + 1 == len(path):
+        if N+1 == len(path):
             return path
-
-        for idx, value in enumerate(routes[key]):
+        
+        for idx, value in enumerate(routes[key]):            
             routes[key].pop(idx)
 
             pt = path[:]
             pt.append(value)
 
-            # 
             rt = dfs(value, pt)
 
-            # 
             if rt:
                 return rt
             
-            # 
             routes[key].insert(idx, value)
-    
-    # 
-    routes = init_path()
+
+
+    routes = init_gragh()
     for r in routes:
         routes[r].sort()
     
-    N = len(tickets)    
+    N = len(tickets)
     answer = dfs('ICN', ['ICN'])
 
-    
     return answer
+
 
 # start from ICN
 tickets = [["ICN", "BBB"], ["ICN", "AAA"], ["BBB", "ICN"]]
-solution(tickets)
+print(solution(tickets))
 
 
