77f368f093bca4dae518a8ae86a538785e9c1800
23Dragon <lsy1819@gamil.com>
2021-03-15 15:11:01 +0900

    0315

--- BRUTEFORCE_Carpet.py
+++ BRUTEFORCE_Carpet.py
@@ -1,3 +1,17 @@
 def solution(brown, yellow):
     answer = []
+
+    tot = brown + yellow
+
+    for i in range(2, tot):
+        if tot % i == 0:
+            x = i           # width
+            y = tot // i    # heigh
+            
+            # width >= heigh
+            if x >= y and (x*2 + y*2 -4) == brown:
+                answer = [x, y]
+
     return answer
+
+solution(24,24)
