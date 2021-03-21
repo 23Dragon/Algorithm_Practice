36e5808faa542aa06041c476a1094f037216187c
23Dragon <lsy1819@gmail.com>
2021-03-21 23:16:20 +0900

    test

---
+++ Baekjoon/Test.py
@@ -0,0 +1,6 @@
+_input = input()
+
+_input = _input.split(' ')
+
+_output = [x for x in _input if x != ""]
+print(len(_output))
---
+++ CodeUp_Basic100/Test.py
@@ -0,0 +1,6 @@
+n = input()
+_inputs = input().split()
+
+_list = [int(x) for x in _inputs]
+
+print(min(_list), max(_list))
