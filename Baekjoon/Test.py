c395a489a0edb6407132a74f51714998bd1f306f
23Dragon <lsy1819@gmail.com>
2021-03-22 22:56:05 +0900

    test

--- Baekjoon/Test.py
+++ Baekjoon/Test.py
@@ -1,6 +1,28 @@
-_input = input()
+n = int(input())
+_inputs = []
+for i in range(n):
+    _inputs.append(input())
 
-_input = _input.split(' ')
 
-_output = [x for x in _input if x != ""]
-print(len(_output))
+answer = 0
+for i in _inputs:    
+    tmp = []
+    for idx, value in enumerate(i):
+        if value not in tmp:
+            tmp.append(value)
+        else:
+            if i[idx-1] == value:
+                tmp.append(value)
+                if idx == len(i)-1:
+                    print(idx, value)
+                    answer += 1
+            else:
+                break
+        
+
+print(answer)
+
+
+
+
+        
