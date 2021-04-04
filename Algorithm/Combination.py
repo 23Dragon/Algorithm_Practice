56eec48e32d9ce3481a3c7103f60aa8d2abc0c6c
23Dragon <lsy1819@gmail.com>
2021-04-04 22:17:45 +0900

    combination

--- Algorithm/Combination.py
+++ Algorithm/Combination.py
@@ -1,28 +1,13 @@
-def main():
-    # nCr    
-    n, r = 3, 2    
-    arr = [x for x in range(n)]
-    result = [0] * r    
-    
-    def combi(level, begin):
-        if level == r:
-            print(result)
-            return
-        for i in range(begin, n):
+def combi(level, start):
+    if level == r:
+        print(result)
+    else:
+        for i in range(start, n):
             result[level] = arr[i]
-            print('level, i, result : ', level, i, result)
             combi(level+1, i+1)
 
-
-        
-
-    combi(0, 0)
-
-    
-
-
-
-
-
 if __name__ == "__main__":
-    main()
+    n, r = 3, 2
+    arr = [x for x in range(n)]
+    result = [0] * r
+    combi(0, 0) # level, start
