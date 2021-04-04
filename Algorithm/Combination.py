da05c93e422f06f0d90535f08a00902092fac4aa
23Dragon <lsy1819@gmail.com>
2021-04-04 16:53:56 +0900

    ttt

---
+++ Algorithm/Combination.py
@@ -0,0 +1,28 @@
+def main():
+    # nCr    
+    n, r = 3, 2    
+    arr = [x for x in range(n)]
+    result = [0] * r    
+    
+    def combi(level, begin):
+        if level == r:
+            print(result)
+            return
+        for i in range(begin, n):
+            result[level] = arr[i]
+            print('level, i, result : ', level, i, result)
+            combi(level+1, i+1)
+
+
+        
+
+    combi(0, 0)
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- CodeUp_Basic100/Test.py
+++
@@ -1,6 +0,0 @@
-n = input()
-_inputs = input().split()
-
-_list = [int(x) for x in _inputs]
-
-print(min(_list), max(_list))
