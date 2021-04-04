99b3d338a62b2c27494c880adce1a194180bd074
23Dragon <lsy1819@gmail.com>
2021-04-04 23:04:06 +0900

    permu

---
+++ Algorithm/Permutation.py
@@ -0,0 +1,18 @@
+def permu(level):
+    if level == r:
+        print(result)        
+    else:
+        for i in range(n):
+            if check[i] == 0:                    
+                result[level] = arr[i]
+                check[i] = 1
+                permu(level+1)
+                check[i] = 0
+
+if __name__ == "__main__":
+    n, r = 3, 2
+    arr = [x for x in range(n)]
+    result = [0] * r
+    check = [0] * n
+
+    permu(0) # level
