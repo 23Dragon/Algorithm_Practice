e37ea3d0c5aef44f330e2a0e34f19479f4255ffd
23Dragon <lsy1819@gamil.com>
2021-04-08 13:00:33 +0900

    test

--- Baekjoon/7568_Dungchi.py
+++ Baekjoon/7568_Dungchi.py
@@ -1,6 +1,19 @@
 
 def solve(n, array):
+    rank_list = list()
     
+    for i in array:
+        rank = 1       
+
+        for j in array:
+            if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+        
+        print(rank, end=' ')
+        
+        
+    
+                
 
 if __name__ == "__main__":
     N = int(input())
