468948fbdeccd6103fe4665a3e4bac44597f3b57
23Dragon <wotnwogus@naver.com>
2022-09-06 12:59:38 +0900

    20220906

--- Baekjoon/4948_BertrandsPostulate.py
+++ Baekjoon/4948_BertrandsPostulate.py
@@ -1,3 +1,26 @@
+import math
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+
 if __name__ == "__main__":
-    n = int(input())
-    
+    while True:        
+        n = int(input())
+        if n == 0:
+            break
+        
+        count = 0
+        # for i in range(n+1, 2*n+1):
+            
+        #     for j in range(2, i):
+        #         if i % j == 0:
+                    
+            
+            
+        print(count)
+        
